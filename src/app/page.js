cbe6001020b3c3ee4089c076164c1b6670d65fdc
celiamartinelli <celia.martinelli@oclock.school>
2024-11-20 13:05:02 +0100

    add-footer-and-main

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./compoment/Header/Header";
+import Footer from "./compoment/Footer/Footer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Next.js</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
